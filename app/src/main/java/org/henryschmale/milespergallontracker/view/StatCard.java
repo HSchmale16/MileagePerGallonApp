52cf7854d3a4c4dc87781e516fc75309924d1ca1
Henry Schmale <hschmale16@gmail.com>
2019-06-15 18:39:57 -0400

    Work on the dashboard.

---
+++ app/src/main/java/org/henryschmale/milespergallontracker/view/StatCard.java
@@ -0,0 +1,67 @@
+package org.henryschmale.milespergallontracker.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+
+import org.henryschmale.milespergallontracker.R;
+
+public class StatCard extends CardView {
+    private TextView statText;
+    private TextView descriptionText;
+    private ImageView iconView;
+
+    public StatCard(@NonNull Context context) {
+        super(context);
+        // init();
+    }
+
+    public StatCard(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public StatCard(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+
+    private void init(AttributeSet attrs) {
+        TypedArray a = getContext().obtainStyledAttributes(
+                attrs,
+                R.styleable.StatCard
+        );
+
+
+
+        inflate(getContext(), R.layout.card_stat, this);
+
+        this.statText = findViewById(R.id.stat);
+        this.descriptionText = findViewById(R.id.description);
+        this.iconView = findViewById(R.id.icon);
+
+        this.iconView.setImageDrawable(
+                getResources().getDrawable(a.getResourceId(
+                        R.styleable.StatCard_iconDrawable,
+                        R.drawable.ic_launcher_background))
+        );
+        this.descriptionText.setText(
+                getResources().getText(a.getResourceId(
+                        R.styleable.StatCard_description,
+                        R.string.def_statcard_description_lbl))
+        );
+
+        a.recycle();
+    }
+
+
+}
